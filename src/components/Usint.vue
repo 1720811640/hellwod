ef4550c55e840aff14ed54b287bd762859f4fef9
1720811640 <1720811640@qq.com>
2019-03-18 21:11:23 +0800

    第一次提交

---
+++ src/components/Usint.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<h2>添加博客</h2>
+		<form>
+			<label>博客标题：</label>
+			<input type="text" v-model="blog.title" required />
+			<label>博客内容</label>
+			<textarea v-model="blog.content"></textarea>
+		</form>
+		<hr>
+		<div id="previewe">
+			<h3>博客总览</h3>
+			<p>博客标题：{{blog.title}}</p>
+			<p>博客内容:</p>
+			<p>{{blog.content}}</p>
+		</div>
+	</div>
+</template>
+<script type="text/javascript">
+export default {
+	name:"usint",
+	data(){
+		return{
+		blog:{
+			title:"",
+			content:""
+		}
+	}
+	}
+}
+</script>
